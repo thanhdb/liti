330854f29a138a9d24734d75a8ae5bc5d58fb226
harley <dobaothanh.tn1@gmail.com>
2017-12-19 23:21:13 +0700

    update

--- config.php
+++ config.php
@@ -6,4 +6,4 @@
 $dm = 'http://liti-liti.1d35.starter-us-east-1.openshiftapps.com'; // ko có kí tự / ở cuối nhé
 
 
-$uid = '100023462092221'; // id facebook của bạn
+$uid = '100023513628078'; // id facebook của bạn
